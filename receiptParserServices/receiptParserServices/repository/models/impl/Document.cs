6899bf0cce1de3d492c7c021dd03a211fa9d2e97
David <lucerodavid@att.net>
2024-07-28 18:23:08 -0500

    never committed these so had to create them again on another branch. committing for historic and tracking purposes

---
+++ receiptParserServices/receiptParserServices/repository/models/impl/Document.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using receiptParserServices.repository.models.inter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace receiptParserServices.repository.models.impl
+{
+    internal class Document : IDocument
+    {
+        public ObjectId Id { get; set; }
+
+        public DateTime CreatedAt => Id.CreationTime;
+    }
+}
---
+++ receiptParserServices/receiptParserServices/repository/models/inter/IDocument.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace receiptParserServices.repository.models.inter
+{
+    internal interface IDocument
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        ObjectId Id { get; set; }
+
+        DateTime CreatedAt { get; }
+    }
+}
