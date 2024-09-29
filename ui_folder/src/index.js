9ec494d78343f9457f71c0076a3ebd4fedc0705c
David Lucero <lucerodavid@att.net>
2024-09-29 11:12:06 -0500

    inital commit for PWA, turns out you can't use NextJS with Create React App, or at least its not recommended

--- ui_folder/src/index.js
+++ ui_folder/src/index.js
@@ -3,17 +3,42 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// import Head from 'next/head';
 
 console.log("NODE_ENV: " + process.env.NODE_ENV);
 console.log("Secret Key: " + process.env.REACT_APP_SECRET_KEY);
 console.log("API URL: " + process.env.REACT_APP_BACKEND_API_URL);
 
+// Layout Component
+// const Layout = ({ children }) => {
+//     return (
+//         <>
+//             <Head>
+//                 <title>My PWA</title>
+//                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+//                 <link rel="manifest" href="/manifest.json" />
+//                 <link rel="icon" href="/favicon.ico" />
+//                 <meta name="theme-color" content="#000000" />
+//             </Head>
+//             <header>
+//                 {/* Add your header content here */}
+//             </header>
+//             <main>
+//                 {children}
+//             </main>
+//             <footer>
+//                 {/* Add your footer content here */}
+//             </footer>
+//         </>
+//     );
+// };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        
+            <App />
+        
     </React.StrictMode>
 );
 
